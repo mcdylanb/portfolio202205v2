929b221cf8b4dc6e5977b220d081af557eba26ad
dylan <dylansatgalab@gmail.com>
2022-05-21 10:46:22 +0000

    adds keyframe animation to Hero Header

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
       <Seo title="Home" />
       <div className="Hero">
         <div className="HeroGroup">
-          <h1>Your now Web Developer</h1> 
-           <h1>for your now project</h1>
+          <h1>Your NOW <br/> Web Developer</h1>
+           <h1>For Your NOW  <br/> Project</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur venenatis nibh eget tincidunt lobortis. Ut at ipsum quis lectus pellentesque aliquam sed at turpis. In ipsum enim, scelerisque sed odio eget, mattis sollicitudin erat. Suspendisse potenti. </p>
           <a href="#"> Lets Start </a>
         </div>
