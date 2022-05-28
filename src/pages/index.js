32dcd27b640f7cbe8b3af43082829c963b7abc95
dylan <dylansatgalab@gmail.com>
2022-05-29 02:40:29 +0800

    Returns the classes from previous revisions

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,19 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
+import "./../components/index.css"
 
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <div className={styles.textCenter}>
-      <h1>Your now Web Developer for your now project</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur venenatis nibh eget tincidunt lobortis. Ut at ipsum quis lectus pellentesque aliquam sed at turpis. In ipsum enim, scelerisque sed odio eget, mattis sollicitudin erat. Suspendisse potenti. </p>
-      <button>Lets Start</button>
-      <button>Learn More</button>
+      <div className="Hero">
+          <div className="HeroGroup">
+              <h1>Your now Web Developer for your now project</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur venenatis nibh eget tincidunt lobortis. Ut at ipsum quis lectus pellentesque aliquam sed at turpis. In ipsum enim, scelerisque sed odio eget, mattis sollicitudin erat. Suspendisse potenti. </p>
+              <button>Lets Start</button>
+              <button>Learn More</button>
+          </div>
     </div>
   </Layout>
 )
